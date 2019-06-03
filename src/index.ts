e3ca65b0b7c234947ffca4b41b40cb4621a703fb
end5 <16655171+end5@users.noreply.github.com>
2019-06-02 18:54:06 -0700

    Fix wording

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const lastBreakingVersion = "27";
 document.addEventListener("DOMContentLoaded", () => {
     const disclaimer = document.createElement("div");
     disclaimer.className = "disclaimer content dark";
-    disclaimer.innerHTML = "<h2>CoC2 Save Editor v" + editorVersion + "</h2><h4>For use with versions of CoC2 " + gameVersion + " or higher</h4><p>Disclaimer: Editing any saves may permanently mess up the save or cause unusual bugs or errors in the game. Edit at your own risk and remember to back up your saves.</p><p>Saves edited with v" + lastBreakingVersion + " or earlier will not work.</p>";
+    disclaimer.innerHTML = "<h2>CoC2 Save Editor v" + editorVersion + "</h2><h4>For use with public versions of CoC2 " + gameVersion + " or higher</h4><p>Disclaimer: Editing any saves may permanently mess up the save or cause unusual bugs or errors in the game. Edit at your own risk and remember to back up your saves.</p><p>Saves edited with v" + lastBreakingVersion + " or earlier will not work.</p>";
     document.body.appendChild(disclaimer);
 
     const ok = document.createElement("button");
