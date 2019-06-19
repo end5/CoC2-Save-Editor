09c36ac159bf89972ad4737b98c3c2a30cef1239
end5 <16655171+end5@users.noreply.github.com>
2019-06-18 20:25:28 -0700

    Fix flag generation

--- scripts/generators.js
+++ scripts/generators.js
@@ -59,6 +59,13 @@ stringifyGlobals({
     Perks: Object.keys(window.PERKS).filter(key => key !== 'default'),
 });
 
+// Pregnancy flags
+Object.keys(window.GLOBALS)
+    .filter(key => key.startsWith('PREG_'))
+    .map(key => window.GLOBALS[key])
+    .filter(key => key)
+    .sort(key => window.GLOBALS[key])
+
 // Command for serializing important chars
 JSON.stringify(Object.keys(window.CHARS)
     .filter(name => name !== 'champ')
@@ -68,11 +75,16 @@ JSON.stringify(Object.keys(window.CHARS)
     }, {})
 );
 
-// Code for getting flags. Only works on file.
+// Code for getting flags
+
+// For file
 var fs = require('fs');
+var contents = fs.readFileSync('./source.js', 'utf8');
+
+// For file open in browser
+var contents = document.body.getElementsByTagName('pre')[0].textContent;
 
 var list = [];
-var contents = fs.readFileSync('./source.js', 'utf8');
 var matches = contents.match(/flags\.[\w_]+/g);
 if (matches && matches.length > 0) {
     list = list.concat(matches.map((value) => value.substr(6)));
@@ -81,12 +93,43 @@ var matches = contents.match(/incFlags\('[\w_]+/g);
 if (matches && matches.length > 0) {
     list = list.concat(matches.map((value) => value.substr(10)));
 }
+
+// This needs to be filled in from pregnancy flags above
+const pregFlagKeys = ["WINGLEADER_PREG", "KASYRRA_PC_PREG", "IMP_PC_PREG", "ALRAUNE_PC_PREG", "HORNET_PC_PREG", "GWYN_PREG", "WYVERN_PREG", "OVILIXER_PC_PREG", "SHAR_OVILIXER_PREG", "LUPINE_PC_PREG", "BRINT_PC_PREG"];
+
+const expandedPregFlags = [];
+for (const flag of pregFlagKeys) {
+    expandedPregFlags.push(flag + '_TIMER');
+    expandedPregFlags.push(flag + '_EGG_LAID');
+    expandedPregFlags.push(flag + '_NUM_BIRTHS');
+    expandedPregFlags.push(flag + '_NUM_KIDS');
+}
+
+list = list.concat(
+    expandedPregFlags,
+    [
+        'HORNET_PC_PREG_EGGS',
+        'GWYN_PREG_LITTER',
+        'WYVERN_PREG_BAB',
+        'WYVERN_PREG_GOT_EGG',
+        'OVILIXER_PC_PREG_BAB',
+        'OVILIXER_PC_PREG_OVIP',
+        'LUPINE_PC_PREG_LITTER',
+        'LUPINE_PC_PREG_GARRET',
+        'LUPINE_PC_PREG_GARRET_TOTAL',
+        'BRINT_PC_PREG_TALKED',
+        'BRINT_PC_PREG_LITTER',
+    ]
+);
+
 list = list
     .filter((value, index, self) => self.indexOf(value) === index && value.toUpperCase() === value)
-    .sort()
-    .map((value) => '"' + value + '"');
+    .sort();
 
-fs.writeFile('./CompressedFlags.js', 'export const Flags = [' + list.join(', ') + '];\n', function (err) {
+const listStr = JSON.stringify(list);
+
+// For file
+fs.writeFile('./Flags.ts', 'export const Flags = ' + listStr + ';\n', function (err) {
     if (err) return console.log(err);
 
     console.log(list.length + " flags were saved!");
