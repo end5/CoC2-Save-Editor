00d8b1770f7a0478e5b5b76b69c3d95372ace254
end5 <16655171+end5@users.noreply.github.com>
2020-11-27 15:20:51 -0800

    New stuff 0.2.40 to 0.3.24

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import "./Window";
 import { displayDisclaimer } from "./UI/Disclaimer";
 
-const editorVersion = "39";
-const gameVersion = "0.2.40";
-const lastBreakingVersion = "27";
+const editorVersion = "40";
+const gameVersion = "0.3.24";
+const lastBreakingVersion = "40";
 
 document.addEventListener("DOMContentLoaded", () => {
     document.body.appendChild(displayDisclaimer(editorVersion, gameVersion, lastBreakingVersion));
