e2c1267249106c7e28b138e5624dc6ed20f4b52f
end5 <16655171+end5@users.noreply.github.com>
2021-12-11 00:11:27 -0800

    Fix Cait's variable changes

--- src/UI/EditorContent/CharContent/BodyContent/Body.ts
+++ src/UI/EditorContent/CharContent/BodyContent/Body.ts
@@ -23,8 +23,8 @@ export function displayBodyContent(getChar: () => CharType) {
             new Label('Height', new NumberField(createValueLookup(getChar, 'tallness'))),
             new Label('Tone', new NumberField(createValueLookup(getChar, 'tone'))),
             new Label('Thickness', new NumberField({
-                get: () => getChar().thickness == null ? getChar().thickness : getChar()._thickness,
-                set: (value) => getChar().thickness == null ? getChar().thickness = value : getChar()._thickness = value,        
+                get: () => getChar().thickness != null ? getChar().thickness : getChar()._thickness,
+                set: (value) => getChar().thickness != null ? getChar().thickness = value : getChar()._thickness = value,        
             })),
         ]
     }, {
@@ -52,8 +52,8 @@ export function displayBodyContent(getChar: () => CharType) {
         title: 'Belly',
         list: [
             new Label('Size Raw', new NumberField({
-                get: () => getChar().bellyRatingRaw == null ? getChar().bellyRatingRaw : getChar()._bellyRatingRaw,
-                set: (value) => getChar().bellyRatingRaw == null ? getChar().bellyRatingRaw = value : getChar()._bellyRatingRaw = value,        
+                get: () => getChar().bellyRatingRaw != null ? getChar().bellyRatingRaw : getChar()._bellyRatingRaw,
+                set: (value) => getChar().bellyRatingRaw != null ? getChar().bellyRatingRaw = value : getChar()._bellyRatingRaw = value,        
             })),
             new Label('Size Mod', new NumberField(createValueLookup(getChar, 'bellyRatingMod'))),
         ]
@@ -61,8 +61,8 @@ export function displayBodyContent(getChar: () => CharType) {
         title: 'Hips',
         list: [
             new Label('Size Raw', new NumberField({
-                get: () => getChar().hipRatingRaw == null ? getChar().hipRatingRaw : getChar()._hipRatingRaw,
-                set: (value) => getChar().hipRatingRaw == null ? getChar().hipRatingRaw = value : getChar()._hipRatingRaw = value,        
+                get: () => getChar().hipRatingRaw != null ? getChar().hipRatingRaw : getChar()._hipRatingRaw,
+                set: (value) => getChar().hipRatingRaw != null ? getChar().hipRatingRaw = value : getChar()._hipRatingRaw = value,        
             })),
             new Label('Size Mod', new NumberField(createValueLookup(getChar, 'hipRatingMod'))),
         ]
@@ -86,8 +86,8 @@ export function displayBodyContent(getChar: () => CharType) {
             })),
             new Label('Color', new StringField(createValueLookup(getButt, '_color'))),
             new Label('Size Raw', new NumberField({
-                get: () => getChar().buttRatingRaw == null ? getChar().buttRatingRaw : getChar()._buttRatingRaw,
-                set: (value) => getChar().buttRatingRaw == null ? getChar().buttRatingRaw = value : getChar()._buttRatingRaw = value,        
+                get: () => getChar().buttRatingRaw != null ? getChar().buttRatingRaw : getChar()._buttRatingRaw,
+                set: (value) => getChar().buttRatingRaw != null ? getChar().buttRatingRaw = value : getChar()._buttRatingRaw = value,        
             })),
             new Label('Size Mod', new NumberField(createValueLookup(getChar, 'buttRatingMod'))),
             new Label('Wetness Raw', new NumberField(createValueLookup(getButt, 'wetnessRaw'))),
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import "./Window";
 import { displayDisclaimer } from "./UI/Disclaimer";
 
-const editorVersion = "52";
+const editorVersion = "53";
 const gameVersion = "0.4.23";
-const lastBreakingVersion = "52";
+const lastBreakingVersion = "53";
 
 document.addEventListener("DOMContentLoaded", () => {
     document.body.appendChild(displayDisclaimer(editorVersion, gameVersion, lastBreakingVersion));
