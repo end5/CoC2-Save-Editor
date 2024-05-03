e094988bdf83d750908040ff40c62d2add3e0d9d
end5 <16655171+end5@users.noreply.github.com>
2024-05-03 15:47:53 -0700

    New stuff 0.6.32 to 0.7.6

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import "./Window";
 import { displayDisclaimer } from "./UI/Disclaimer";
 
-const editorVersion = "65";
-const gameVersion = "0.6.32";
+const editorVersion = "66";
+const gameVersion = "0.7.6";
 const lastBreakingVersion = "62";
 
 document.addEventListener("DOMContentLoaded", () => {
